4d470ba009bd80709130f61977e98762e4443149
AcE <goth14wx@gmail.com>
2019-11-19 20:33:42 -0600

    Updates

---
+++ precache-manifest.512a5f9cf76043f37eec2a0576368b24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84343dc523b56d9e634a8e1521286110",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "3f5120aa5ad114c8cb93",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7a87a04afad9ed345eff",
+    "url": "/react-gh-pages/static/js/2.edc668c8.chunk.js"
+  },
+  {
+    "revision": "3f5120aa5ad114c8cb93",
+    "url": "/react-gh-pages/static/js/main.573a3f19.chunk.js"
+  },
+  {
+    "revision": "0cfbb5327de4f44d09c8",
+    "url": "/react-gh-pages/static/js/runtime-main.a93c4905.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
